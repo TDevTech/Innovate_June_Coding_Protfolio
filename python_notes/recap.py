36cfbea87bc8d582ce7999a1f3e69d3446308d32
TARIG AHMED <tdev4tech@gmail.com>
2022-06-27 17:05:50 +0100

    added python notes folder

---
+++ python_notes/recap.py
@@ -0,0 +1,20 @@
+print ("This is Python Recap Notes")
+
+greeting = "Wellcom to L3 Innovate Coding!!!"
+
+print (greeting)
+
+print ("This a string for displaying characters 123, @~&, !!!")
+
+print ("This Numbers: 0, 1, 2, 3, ... 1000 !!!")
+
+print (0, 1, 2, 3, + 1000)
+
+print (1.63* 10)
+
+print (True)
+
+print (False)
+
+print (None)
+
