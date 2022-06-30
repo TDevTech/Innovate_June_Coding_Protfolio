42cc2ddba3f231e40e23d5408ddb20a7573ed977
TARIG AHMED <tdev4tech@gmail.com>
2022-06-30 04:16:06 +0100

    .py updated on 30/06/022@0415

--- dictionary_notes.py
+++ dictionary_notes.py
@@ -35,21 +35,21 @@
 
 ## Activity 2
 
-countrys = {
-    "United Kingdom" : "London",
-    "France" :"Paris",
-    "Germany" : "Berlin",
-    "Spain" : "Madrid",
-    "Italy" : "Rome"}
+# countrys = {
+#     "United Kingdom" : "London",
+#     "France" :"Paris",
+#     "Germany" : "Berlin",
+#     "Spain" : "Madrid",
+#     "Italy" : "Rome"}
 
-print(countrys)
+# print(countrys)
 
-China = countrys.setdefault('China','Bejen')
-canada = countrys.setdefault('Canada' ,'Ottowa')
-print('countrys =',countrys)
-print('China =' ,China)
-print('canada =', canada)
-print (countrys)
+# China = countrys.setdefault('China','Bejen')
+# canada = countrys.setdefault('Canada' ,'Ottowa')
+# print('countrys =',countrys)
+# print('China =' ,China)
+# print('canada =', canada)
+# print (countrys)
 
 #countrys.setdefault ({'China': 'Bejen', 'Canada': 'Ottawa'})
 #print(countrys)
@@ -58,21 +58,79 @@ print (countrys)
 
 # keys: artist, song_name, genre, release_year
 
-fav_songs = [
-    {"George Benson":"Affirmation" : "Smooth Jazz":"1976",
-    "Bob James":"Since I Fell For You":"Smooth Jazz":"1986",
-    "Bob James" : "Raise The Roof" : "Smooth Jazz" : "1999",
-    "Bony James" : "Joy Ride" : "Smooth Jazz" : "1999", 
-    "Ron Fattorusso" : "No Es Facil" : "Latin Smooth Jazz" : "2004",
-    "Incognito" : "Pieces Of A Dream" : "Smooth Jazz" : "1993",
-    "Sade" : "Your Love Is King" : "Smooth Jazz & soul" : "1983",
-    "Chris Botti" : "Good Morning Heartache" : "Jazz & RnB" : "2005",
-    "Norman Brown" : "After The Storm" : "Smooth Jazz" : "1994",
-    "Grover Washington Jr" : "Take Five" : "Smooth Jazz" : "1992"}
-]
+# fav_songs = {
+#     "George Benson":"Affirmation," "Smooth Jazz," "1976",
+#     "Bob James":"Since I Fell For You":"Smooth Jazz":"1986",
+#     "Bob James":"Raise The Roof":"Smooth Jazz":"1999",
+#     "Bony James":"Joy Ride":"Smooth Jazz":"1999", 
+#     "Ron Fattorusso": "No Es Facil" : "Latin Smooth Jazz" : "2004",
+#     "Incognito":"Pieces Of A Dream" : "Smooth Jazz" : "1993",
+#     "Sade":"Your Love Is King" : "Smooth Jazz & soul" : "1983",
+#     "Chris Botti":"Good Morning Heartache" : "Jazz & RnB" : "2005",
+#     "Norman Brown":"After The Storm" : "Smooth Jazz" : "1994",
+#     "Grover Washington Jr":"Take Five" : "Smooth Jazz" : "1992"}
 
-fav_songs = { 'Artist' : '', 'Song_name': '', 'Genre' : '', 'Release_year' : ''}
-print (fav_songs)
+
+# fav_songs = {
+#             'track00' : [ 'Artist':'null','Song_name':'','Genre' :'','Release_year':''],
+#             'track01' : [ 'Artist':'George Benson','Song_name':'Affirmation','Genre' :'Smooth Jazz','Release_year':'1976'],
+#             'track02' : [ 'Artist':'Bob James','Song_name':'Since I Fell For You','Genre' :'Smooth Jazz','Release_year':'1986'],
+#             'track03' : [ 'Artist':'Bob James','Song_name':'Raise The Roof','Genre' :'Smooth Jazz','Release_year':'1999'],
+#             'track04' : [ 'Artist':'Bony James','Song_name':'Joy Ride','Genre' :'','Release_year':'1999'],
+#             'track05' : [ 'Artist':'Ron Fattorusso','Song_name':'No Es Facil','Genre' :'Latin Smooth Jazz','Release_year':'2004'],
+#             'track06' : [ 'Artist':'Incognito','Song_name':'Pieces Of A Dream','Genre' :'Smooth Jazz','Release_year':'1993'],
+#             'track07' : [ 'Artist':'Sade','Song_name':'Love Is King','Genre' :'Smooth Jazz & soul','Release_year':'1983'],
+#             'track08' : [ 'Artist':'Chris Botti','Song_name':'Good Morning Heartache','Genre' :'Jazz & RnB','Release_year':'2005'],
+#             'track09' : [ 'Artist':'Norman Brown','Song_name':'After The Storm','Genre' :'Smooth Jazz','Release_year':'1994'],
+#             'track10' : [ 'Artist':'Grover Washington Jr','Song_name':'Take Five','Genre' :'Smooth Jazz','Release_year':'1992']
+#             }
+# #import string
+
+# keys = { 'Artist':'','Song_name':'','Genre' :'','Release_year':''}
+
+
+# track00 = {'Artist':'null','Song_name':'none','Genre' :'none','Release_year':'null'} 
+# track01 = { 'Artist':'George Benson','Song_name':'Affirmation','Genre' :'Smooth Jazz','Release_year':'1976'}
+# track02 = { 'Artist':'Bob James','Song_name':'Since I Fell For You','Genre' :'Smooth Jazz','Release_year':'1986'}
+# track03 = { 'Artist':'Bob James','Song_name':'Raise The Roof','Genre' :'Smooth Jazz','Release_year':'1999'}
+# track04 = { 'Artist':'Bony James','Song_name':'Joy Ride','Genre' :'','Release_year':'1999'}
+# track05 = { 'Artist':'Ron Fattorusso','Song_name':'No Es Facil','Genre' :'Latin Smooth Jazz','Release_year':'2004'}
+# track06 = { 'Artist':'Incognito','Song_name':'Pieces Of A Dream','Genre' :'Smooth Jazz','Release_year':'1993'}
+# track07 = { 'Artist':'Sade','Song_name':'Love Is King','Genre' :'Smooth Jazz & soul','Release_year':'1983'}
+# track08 = { 'Artist':'Chris Botti','Song_name':'Good Morning Heartache','Genre' :'Jazz & RnB','Release_year':'2005'}
+# track09 = { 'Artist':'Norman Brown','Song_name':'After The Storm','Genre' :'Smooth Jazz','Release_year':'1994'}
+# track10 = { 'Artist':'Grover Washington Jr','Song_name':'Take Five','Genre' :'Smooth Jazz','Release_year':'1992'}
+
+# fav_songs = [track00, track01, track02]
+# #keys = { 'Artist':'','Song_name':'','Genre' :'','Release_year':''}
+# print (fav_songs)
 
 #print (fav_songs.keys())
 
+# print("Keys before Dictionary Updation:")
+# keys = fav_songs.keys()
+# print(keys)
+
+# fav_songs.update({'Artist':'David Sanborn', 'Song_name':' Maputo', 'Genre':'Smooth Jazz', 'Release_year':'1983'})
+# print ("\n After songs dictionary is updated: ")
+# print (keys)
+
+fav_songs = {'Artist':'George Benson','Song_name':'Affirmation','Genre' :'Smooth Jazz','Release_year':1976 }
+print (fav_songs)
+
+print (fav_songs['Artist'])
+
+print (fav_songs.keys())
+
+fav_songs['Time'] = '6:00'
+print (fav_songs)
+#del fav_songs['Time'] = '6:00'
+#print (fav_songs)
+
+songs_dict = { 
+            "Artist":"Bob James",
+            "Song_name":"Raise The Roof",
+            "Genre" :"Smooth Jazz",
+            "Release_year":1999
+            }
+print (songs_dict)
