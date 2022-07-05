b6c7e3cf7f4d6b492c9caf278866f8f18585cdaa
TARIG AHMED <tdev4tech@gmail.com>
2022-07-05 09:41:23 +0100

    updated on 05/07/2022@09:40

---
+++ oop_python/person.py
@@ -0,0 +1,13 @@
+
+
+class Person():
+    def _init_(self, person_name, age, color):
+        
+        self.name = person_name
+        self.age = age
+        self.color = color
+
+
+Developer_name = Person("Tarig")
+
+print(Developer_name.name)
