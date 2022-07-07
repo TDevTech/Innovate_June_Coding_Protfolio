464734f7c241dea96d2e5422e4362644bc0fb4dd
TARIG AHMED <tdev4tech@gmail.com>
2022-07-07 18:30:04 +0100

    updated on 07/07/2022@18:30

--- backend_project/static/app.js
+++ backend_project/static/app.js
@@ -1,4 +1,93 @@
 
+function darkMode()
+{
+let element = document.body;
+// let element.classList.toggle("dark-mode");
+let mainBox = document.getElementsByClassName ("main-box");
+let mainText = document.getElementsByClassName ("main-text");
+let exampleText = document.getElementsByClassName("example-text");
+let state = localStorage.getItem("state")
+
+
+element.classList.toggle("dark-mode");
+for (const i of mainBox)
+{
+    i.classList.toggle("dark-mode")
+}
+
+for (const x of mainText){
+    x.classList.toggle("dark-mode")
+}
+
+for (const y of exampleText)
+{
+    y.classList.toggle("dark-mode")
+}
+
+if (state !== "dark")
+{
+    localStorage.setItem("state","dark")
+}  
+else
+{
+    localStorage.setItem("state","light")
+}
+
+}
+
+function darkCheck()
+{
+    let element = document.body;
+    // let element.classList.toggle("dark-mode");
+    let mainBox = document.getElementsByClassName ("main-box");
+    let mainText = document.getElementsByClassName ("main-text");
+    let exampleText = document.getElementsByClassName("example-text");
+    let state = localStorage.getItem("state") 
+
+    if (state === "dark")
+    {
+        element.classList.toggle("dark-mode");
+        for (const i of mainBox)
+        {
+            i.classList.toggle("dark-mode")
+        }
+        
+        for (const x of mainText){
+            x.classList.toggle("dark-mode")
+        }  
+    }
+}
+
+// ------------
+// const toggleBtn = document.getElementById("toggle-btn");
+// const theme = document.getElementById("theme");
+// let darkMode = localStorage.getItem("dark-mode");
+// 
+// const enableDarkMode = () => {
+//   theme.classList.add("dark-mode-theme");
+//   toggleBtn.classList.remove("dark-mode-toggle");
+//   localStorage.setItem("dark-mode", "enabled");
+// };
+// 
+// const disableDarkMode = () => {
+//   theme.classList.remove("dark-mode-theme");
+//   toggleBtn.classList.add("dark-mode-toggle");
+//   localStorage.setItem("dark-mode", "disabled");
+// };
+// 
+// if (darkMode === "enabled") {
+//   enableDarkMode(); // set state of darkMode on page load
+// }
+// 
+// toggleBtn.addEventListener("click", (e) => {
+//   darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+//   if (darkMode === "disabled") {
+    // enableDarkMode();
+//   } else {
+    // disableDarkMode();
+//   }
+// });
+// ------------
 
 function changeText(id) {
     id.innerHTML = "you have changed this text with an 'onclick' event"
